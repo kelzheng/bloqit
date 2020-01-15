42a6b53f0b1054a2be0714803829229dcfc3bc5d
Johnnybib <jamesgrahamhu@gmail.com>
2020-01-14 19:25:48 -0500

    dragging gates yay

---
+++ Assets/Scripts/GateDrag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateDrag : MonoBehaviour
+{
+    bool holding = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(holding)
+        {
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            point.z = 0;
+            transform.position = point;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        holding = false;
+    }
+
+}
---
+++ Assets/Scripts/GateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSelector : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject gate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Instantiate(gate, transform.position, Quaternion.identity);
+    }
+
+}
