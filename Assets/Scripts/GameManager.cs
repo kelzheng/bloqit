88d43dfb445cbc7b1b18f0188fc247347c0902d6
Tstorwick <tstorwick@gmail.com>
2020-01-14 19:08:05 -0500

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private BoardManager boardScript;
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
