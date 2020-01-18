6f0fbe98c6a0093a71b8b413af239f6b7033a287
Tstorwick <tstorwick@gmail.com>
2020-01-18 17:57:15 -0500

    Math is entered!

--- Assets/Scripts/MathManager.cs
+++ Assets/Scripts/MathManager.cs
@@ -9,13 +9,15 @@ public class MathManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+        Matrix<double> A = DenseMatrix.OfArray(new double[,] {
         {1,1,1,1},
         {1,2,3,4},
         {4,3,2,1}});
         Vector<double>[] nullspace = A.Kernel();
 
-        Debug.Log((A * (2 * nullspace[0] - 3 * nullspace[1])));*/
+
+
+        Debug.Log((A * (2 * nullspace[0] - 3 * nullspace[1])));
 
     }
 
