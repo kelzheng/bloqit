803d527c73b7f2e7635396039637e8b296978b53
Kelly Zheng <dao@v1040-wn-rt-c-23-191.campus-dynamic.uwaterloo.ca>
2020-01-27 12:31:47 -0500

    gate match, moment tensoring, final state calc

--- Assets/Scripts/BrokenPasta.cs
+++ Assets/Scripts/BrokenPasta.cs
@@ -1,62 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Double;
-using MathNet.Numerics.LinearAlgebra.Complex32;
-
-
-using UnityEngine;
-using MathNet.Numerics;
+﻿using UnityEngine;
+using MathNet.Numerics; // for Complex32 values in gates
+using MathNet.Numerics.LinearAlgebra.Complex32; // for gates
+using System.Collections.Generic; // for List
 
 public class BrokenPasta : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Start() // Method to test Debug.Log()s in unity //
     {
-        // 1. Unary Gates
+        string[,] TestCircuit = { { "x", "cnot", "s", "h", "i", "i" }, { "i", "cnott", "t", "i", "i", "z" } }; // a test circuit
+        var EvaluatedMoments = EvaluateMoment(TestCircuit); // evaluate the moments in the circuit, return one equivalent moments multiplied together
+        var FinalState = CalcState(EvaluatedMoments); // multiply the state by the moments
+        Debug.Log(FinalState); // return the final state
+    }
+
+    public DenseMatrix MatchGate(string gate) // Method to take string gate name -> 2x2 or 4x4 DenseMatrix of equivalent gate //
+    {
+        // 1. Unary Gates, 2x2
         // 1.A Pauli Matricies (rotate on x, y and z matrices 180 degrees)
         // X operator, NOT operator, bit flip, sigma_x
-        var X = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var X = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(0,0),new Complex32(1,0)},
              {new Complex32(1,0),new Complex32(0,0)}});
         // Y operator, sigma_y
-        var Y = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var Y = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(0,0),new Complex32(0,-1)},
              {new Complex32(0,1),new Complex32(0,0)}});
         // Z operator, phase flip operator, flips by pi rad aka 180 deg)
-        var Z = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var Z = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(-1,0)}});
         // I operator, identity
-        var I = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var I = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(1,0)}});
 
         // 1.B Special cases of phase shift operator, R_phi
         // S operator, phi = pi/2, rotate on z axis by 90 deg
-        var S = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var S = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(0,1)}});
         // T operator, phi = pi/2, rotate on z axis by 45 deg
-        var T = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var T = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0)},
              {new Complex32(0,0),Complex32.FromPolarCoordinates(1,Mathf.PI/4)}});
 
         // 1.C Hadamard operator, takes qubit from definite computational basis to a superposition of two states
-        var H = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var H = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32((1/Mathf.Sqrt(2)),0),new Complex32((1/Mathf.Sqrt(2)),0)},
              {new Complex32((1/Mathf.Sqrt(2)),0),new Complex32(-(1/Mathf.Sqrt(2)),0)}});
 
-        // 2. Binary Operators
+        // 2. Binary Operators, 4x4
         // 2.A Swap operator, swaps states of two qubits
-        var SWAP = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var SWAP = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(1,0)}});
         // 2.B CNOT operator, if control qubit (1st q) is 0, do nothing to target qubit (2nd q),
         // but if control qubit is 1, apply NOT operator to target qubit
-        var CNOT = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var CNOT = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(1,0)},
@@ -64,7 +66,7 @@ public class BrokenPasta : MonoBehaviour
         // 2.C CZ operator, if control qubit (1st q) is 0, do nothing to target qubit (2nd q),
         // but if control qubit is 1, apply Z operator to target qubit
         // is symmetric so same result if either qubit is target/control
-        var CZ = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
+        var CZ = DenseMatrix.OfArray(new Complex32[,]
             {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
              {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
@@ -72,53 +74,179 @@ public class BrokenPasta : MonoBehaviour
         //Debug.Log((X)); Debug.Log((Y)); Debug.Log((Z)); Debug.Log((S)); Debug.Log((T)); Debug.Log((H));
 
 
-        //        _.-- -._ /\\
-        //    ./ '       "--`\//
-        //  ./              o \
-        // /./\  )______   \__ \
-        //./  / /\ \   | \ \  \ \
-        //   / /  \ \  | |\ \  \7
-        //    "     "    "  "
+        // 3. Match the gate
+        int numQubits = 2;
+        int rows = numQubits;
+        int columns = gate.Length/numQubits;
 
-        // 3. States
-        // try to learn how to tensor multipy from here https://iridium.mathdotnet.com/api/MathNet.Numerics.LinearAlgebra/Matrix.htm
-        var ket0 = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
-            {{new Complex32(1,0)},
-             {new Complex32(0,0)}});
+        if (gate == "x")
+        {
+            return X;
+        }
 
-        //var nood = ket0.TensorMultiply(ket0);
-        //Debug.Log((nood));
+        if (gate == "y")
+        {
+            return Y;
+        }
 
-        //Matrix<double> A = DenseMatrix.OfArray(new double[,] {
-        //{1,1,1,1},
-        //{1,2,3,4},
-        //{4,3,2,1}});
-        //Vector<double>[] nullspace = A.Kernel();
+        if (gate == "z")
+        {
+            return Z;
+        }
 
-        //// verify: the following should be approximately (0,0,0)
-        //Debug.Log((A * (2 * nullspace[0] - 3 * nullspace[1])));
+        if (gate == "i")
+        {
+            return I;
+        }
 
-        //var M = Matrix<double>.Build;
-        //var C = Matrix<Complex32>.Build;
+        if (gate == "s")
+        {
+            return S;
+        }
 
-        //var X = M.DenseOfArray(new[,] {{ 0.0,  1.0 },
-        //                               { 1.0, 0.0 }});
+        if (gate == "t")
+        {
+            return T;
+        }
 
-        //var A = MathNet.Numerics.LinearAlgebra.Complex32.DenseMatrix.OfArray(new Complex32[,]
-        //    {{new Complex32(1,0),new Complex32(0,0)},
-        //      {new Complex32(0,0),new Complex32(1,0)}});
-        //Debug.Log((A));
+        if (gate == "h")
+        {
+            return H;
+        }
 
+        if (gate == "cnot" || gate == "cnott")
+        {
+            return CNOT;
+        }
 
-        //multiplication for later from https://numerics.mathdotnet.com/api/MathNet.Numerics.LinearAlgebra.Complex/Matrix.htm#Multiply
-        //var tom = H.Multiply(H);
-        //Debug.Log((tom));
+        if (gate == "cz" || gate == "czt")
+        {
+            return CZ;
+        }
 
+        if (gate == "swap" || gate == "swapt")
+        {
+            return SWAP;
+        }
+        else
+        {
+            return null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public DenseMatrix EvaluateMoment(string[,] Circuit) // Method to take numQubitsx(Circuit.Length/numQubits) string array of gate names -> 2x2 or 4x4 DenseMatrix of equivalent gate with MatchGate method -> 4x4 DenseMatrix of all moments multiplied together //
     {
-        
+        // Circuit dimensions
+        int numQubits = 2;
+        int rows = numQubits;
+        int columns = Circuit.Length / numQubits;
+
+        // Initialize gates to be tensored
+        var Moments = new List<DenseMatrix>();
+        string gate;
+        DenseMatrix gateOne = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(-1,0)}});
+        DenseMatrix gateTwo = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(-1,0)}});
+        DenseMatrix TensoredMoment = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(-1,0)}});
+        DenseMatrix FinalTensoredMoments = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(1,0)}});
+
+        // Gates for skip condition
+        var SWAP = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(1,0)}});
+        var CNOT = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(1,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)}});
+        var CZ = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0),new Complex32(0,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(1,0),new Complex32(0,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(1,0),new Complex32(0,0)},
+             {new Complex32(0,0),new Complex32(0,0),new Complex32(0,0),new Complex32(-1,0)}});
+
+        // Match gates in each moment and tensor together
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                if (j == 0)
+                {
+                    gate = Circuit[j, i];
+                    gateOne = MatchGate(gate);
+                }
+                if (j == 1)
+                {
+                    gate = Circuit[j, i];
+                    gateTwo = MatchGate(gate);
+                }
+
+            }
+            // Skip condition for SWAP, CNOT, CZ
+            if (gateOne.Equals(SWAP) || gateOne.Equals(CNOT) || gateOne.Equals(CZ) || gateTwo.Equals(SWAP) || gateTwo.Equals(CNOT) || gateTwo.Equals(CZ))
+            {
+                TensoredMoment = gateOne;
+                Moments.Add(TensoredMoment);
+            }
+            else
+            {
+                TensoredMoment = (DenseMatrix)gateOne.KroneckerProduct(gateTwo);
+                Moments.Add(TensoredMoment);
+            }
+        }
+
+        // Multiply together all moments for FinalTensoredMoments
+        for (int idx = 0; idx < rows; idx++)
+        {
+            FinalTensoredMoments = (DenseMatrix)FinalTensoredMoments.Multiply(Moments[idx]);
+        }
+        return FinalTensoredMoments;
+    }
+
+
+    public DenseMatrix CalcState(DenseMatrix FinalTensoredMoments) // Method to take 4x4 DenseMatrix of FinalTensoredMoments -> 4x1 DenseMatrix of final state, assuming |00> initial state
+    {
+        // States
+        // 1-qubit basis states <0| and <1|
+        var bra0 = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0), new Complex32(0,0) }});
+        var bra1 = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(0,0), new Complex32(1,0) }});
+        // Some kets |0> and |1>
+        var ket0 = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(1,0)},
+         {new Complex32(0,0)}});
+        var ket1 = DenseMatrix.OfArray(new Complex32[,]
+            {{new Complex32(0,0)},
+         {new Complex32(1,0)}});
+        // 2-qubit basis states |00> |11> |01> |10>
+        // note: Kronecker Product is tensor-product
+        var ket00 = ket0.KroneckerProduct(ket0);
+        var ket11 = ket1.KroneckerProduct(ket1);
+        var ket01 = ket0.KroneckerProduct(ket1);
+        var ket10 = ket1.KroneckerProduct(ket0);
+
+        // Mutltiply state by FinalTensoredMoments
+        var state = (DenseMatrix)FinalTensoredMoments.Multiply(ket00);
+
+        // Return final state
+        return state;
     }
 }
